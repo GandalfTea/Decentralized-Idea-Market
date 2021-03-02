c373f41904623a288547202aa6948ac6c12472fa
Octavian Rusu <octavian.concept@gmail.com>
2021-03-02 11:54:37 -0700

    Deleting useless code. Starting over.

---
+++ src/orderDef.cpp
@@ -0,0 +1,20 @@
+
+
+//Orders should contain:
+// * Addresss of user
+// * Timestamp
+// * Security
+// * Price
+// * Quantity
+// * Type: sell of buy
+// * Expiration
+// * Execution type
+// * Locked money or security
+
+
+
+// Structure order into order flow
+// Create transactions
+// Update state
+// Validate transactions
+// Create genesis block
